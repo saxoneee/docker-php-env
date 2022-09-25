7c8946e957e00ba7cc03fd55cee5218f0fe375df
Hendrik Sachse <hendrik.sachse@gmail.com>
2022-09-25 20:05:37 +0200

    add vim and additional project php ini

--- docker/php-server/entrypoint.sh
+++ docker/php-server/entrypoint.sh
@@ -6,6 +6,9 @@ rm -rf /etc/apache2/sites-enabled/*
 # symlink vhost config
 ln -s /home/cfg/apache/project-vhost.conf /etc/apache2/sites-enabled/project-vhost.conf
 
+# symlink project php.ini
+ln -s /home/cfg/php/project-php.ini /etc/php/7.4/apache2/conf.d/9999-project-php.ini
+
 # enable ssl
 a2enmod ssl
 
